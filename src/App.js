f1983ccf5baf6ecdfa091f2212e3d745345b4c1a
Sanket Joshi <nishant.joshi1109@gmail.com>
2023-09-07 14:09:08 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
         <Navbar />
         
         <Routes>
-          <Route path='https://nishant6825.github.io/news-seeker/' element={<News country={'in'} category={"general"} pageSize={6} apiKey={`e14924ccd91b44688321807adc1d0900`} page={1}/>}></Route>
-          <Route exact path="https://nishant6825.github.io/news-seeker/Business" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'business'}/>}/>
-          <Route exact path="https://nishant6825.github.io/news-seeker/Entertainment" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'entertainment'}/>}/>
-          <Route exact path="https://nishant6825.github.io/news-seeker/Health" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'health'}/>}/>
-          <Route exact path="https://nishant6825.github.io/news-seeker/Science" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'science'}/>}/>
-          <Route exact path="https://nishant6825.github.io/news-seeker/Sports" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'sports'}/>}/>
-          <Route exact path="https://nishant6825.github.io/news-seeker/Technology" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'technology'}/>}/>
+          <Route path='/https://nishant6825.github.io/news-seeker/' element={<News country={'in'} category={"general"} pageSize={6} apiKey={`e14924ccd91b44688321807adc1d0900`} page={1}/>}></Route>
+          <Route exact path="/https://nishant6825.github.io/news-seeker/Business" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'business'}/>}/>
+          <Route exact path="/https://nishant6825.github.io/news-seeker/Entertainment" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'entertainment'}/>}/>
+          <Route exact path="/https://nishant6825.github.io/news-seeker/Health" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'health'}/>}/>
+          <Route exact path="/https://nishant6825.github.io/news-seeker/Science" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'science'}/>}/>
+          <Route exact path="/https://nishant6825.github.io/news-seeker/Sports" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'sports'}/>}/>
+          <Route exact path="/https://nishant6825.github.io/news-seeker/Technology" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'technology'}/>}/>
 
         </Routes>
       </Router>
