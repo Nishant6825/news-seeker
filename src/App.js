b2868dc148d2e9049039db1ed6e5b5f19a5fffe5
Sanket Joshi <nishant.joshi1109@gmail.com>
2023-09-11 11:11:31 +0530

    Added snap of project

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ function App() {
     <>
       <Router>
         <Navbar />
-        <Hi/>
+        {/* <Hi/> */}
         
         <Routes>
-          <Route path='/http://Nishant6825.github.io/news-seeker/' element={<News country={'in'} category={"general"} pageSize={6} apiKey={`e14924ccd91b44688321807adc1d0900`} page={1}/>}></Route>
-          <Route exact path="/news-seeker/Business" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'business'}/>}/>
-          <Route exact path="/news-seeker/Entertainment" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'entertainment'}/>}/>
-          <Route exact path="/news-seeker/Health" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'health'}/>}/>
-          <Route exact path="/news-seeker/Science" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'science'}/>}/>
-          <Route exact path="/news-seeker/Sports" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'sports'}/>}/>
-          <Route exact path="/news-seeker/Technology" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'technology'}/>}/>
+          <Route path='/' element={<News country={'in'} category={"general"} pageSize={6} apiKey={`e14924ccd91b44688321807adc1d0900`} page={1}/>}></Route>
+          <Route exact path="/Business" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'business'}/>}/>
+          <Route exact path="/Entertainment" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'entertainment'}/>}/>
+          <Route exact path="/Health" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'health'}/>}/>
+          <Route exact path="/Science" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'science'}/>}/>
+          <Route exact path="/Sports" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'sports'}/>}/>
+          <Route exact path="/Technology" element={<News  apiKey={'e14924ccd91b44688321807adc1d0900'} pageSize={6} country={'in'} category={'technology'}/>}/>
 
         </Routes>
       </Router>
